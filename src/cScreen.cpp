e09fe1627bfca283e695a7557f5ed8dd40fbec09
NathanT <nathantytgat@gmail.com>
2022-11-03 17:05:15 +0100

    Add cScreen class

---
+++ src/cScreen.cpp
@@ -0,0 +1,23 @@
+#include "cScreen.h"
+
+cScreen::cScreen()
+{
+    //ctor
+}
+
+cScreen::~cScreen()
+{
+    //dtor
+}
+
+cScreen::cScreen(const cScreen& other)
+{
+    //copy ctor
+}
+
+cScreen& cScreen::operator=(const cScreen& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
