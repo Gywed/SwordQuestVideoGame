5a0cec7701ce083b044a6338970b69ba6df9a74d
NathanT <nathantytgat@gmail.com>
2022-11-03 18:33:40 +0100

    Implement Run function

--- src/PlayScreen.cpp
+++ src/PlayScreen.cpp
@@ -1,5 +1,23 @@
 #include "PlayScreen.h"
 
+//Sprite speed
+#define SPRITE_SPEED 0.2
+
+// Size of Sprite
+#define Sprite_Width 96
+#define Sprite_Height 104
+
+//// Define not moving sprite for each position
+#define Sprite_Right 312
+#define Sprite_Left 104
+#define Sprite_Back 208
+#define Sprite_Front 0
+//
+//// Define moving sprite for each position
+//#define Sprite_Right_Moving 728
+//#define Sprite_Left_Moving 520
+//#define Sprite_Back_Moving 624
+//#define Sprite_Front_Moving 416
 PlayScreen::PlayScreen(void)
 {
     //ctor
@@ -29,60 +47,119 @@ PlayScreen& PlayScreen::operator=(const PlayScreen& rhs)
 }
 
 int PlayScreen::Run(sf::RenderWindow *App){
-	sf::Event Event;
+
+    // ::: Load texture :::
+
+    // Create texture from PNG file
+    sf::Texture texture;
+    if (!texture.loadFromFile("images/SpriteSheet.png"))
+    {
+        std::cerr << "Error while loading texture" << std::endl;
+        return -1;
+    }
+    texture.setSmooth(true);
+
+    // ::: Create sprite and apply texture :::
+    sf::IntRect rectSourceSprite(0, 0, Sprite_Width, Sprite_Height);
+    sf::Sprite sprite(texture,rectSourceSprite);
+    sprite.setScale(.8,.8);
+    sf::FloatRect spriteSize=sprite.getGlobalBounds();
+    sprite.setOrigin(spriteSize.width/2.,spriteSize.height/2.);
+
+    // ::: Main loop :::
+
+    // Sprite coordinates
+    float x=App->getSize().x/2.;
+    float y=App->getSize().y/2.;
+
+
+	sf::Event ev;
 	bool Running = true;
 
+	//Flags for key pressed
+    bool upFlag=false;
+    bool downFlag=false;
+    bool leftFlag=false;
+    bool rightFlag=false;
+
+    sf::Clock timer;
 	while (Running)
 	{
 		//Verifying events
-		while (App->pollEvent(Event))
+		while (App->pollEvent(ev))
 		{
 			// Window closed
-			if (Event.type == sf::Event::Closed)
+			if (ev.type == sf::Event::Closed)
 			{
+			    App->close();
 				return (-1);
 			}
 			//Key pressed
-			if (Event.type == sf::Event::KeyPressed)
+			if (ev.type == sf::Event::KeyPressed)
 			{
-				switch (Event.key.code)
-				{
-				case sf::Keyboard::Escape:
-					return (0);
-					break;
-				case sf::Keyboard::Up:
-					posy -= movement_step;
-					break;
-				case sf::Keyboard::Down:
-					posy += movement_step;
-					break;
-				case sf::Keyboard::Left:
-					posx -= movement_step;
-					break;
-				case sf::Keyboard::Right:
-					posx += movement_step;
-					break;
-				default:
-					break;
-				}
+				switch (ev.key.code)
+                {
+                // If escape is pressed, close the application
+                case  sf::Keyboard::Escape : App->close(); return (-1);break;
+
+                // Process the up, down, left and right keys
+                case sf::Keyboard::Up :     upFlag=true; break;
+                case sf::Keyboard::Down:    downFlag=true; break;
+                case sf::Keyboard::Left:    leftFlag=true; break;
+                case sf::Keyboard::Right:   rightFlag=true; break;
+                default : break;
+                }
 			}
+			// If a key is released
+            if (ev.type == sf::Event::KeyReleased)
+            {
+                switch (ev.key.code)
+                {
+                // Process the up, down, left and right keys
+                case sf::Keyboard::Up :     upFlag=false; rectSourceSprite.top=Sprite_Back; break;
+                case sf::Keyboard::Down:    downFlag=false; rectSourceSprite.top=Sprite_Front; break;
+                case sf::Keyboard::Left:    leftFlag=false; rectSourceSprite.top=Sprite_Left;break;
+                case sf::Keyboard::Right:   rightFlag=false; rectSourceSprite.top=Sprite_Right;break;
+                default : break;
+                }
+                sprite.setTextureRect(rectSourceSprite);
+            }
 		}
 
-		//Updating
-		if (posx>630)
-			posx = 630;
-		if (posx<0)
-			posx = 0;
-		if (posy>470)
-			posy = 470;
-		if (posy<0)
-			posy = 0;
-		Rectangle.setPosition({ posx, posy });
+        // Update coordinates
+        if (leftFlag){
+            x-=SPRITE_SPEED;
+//            sprite.setTextureRect(sf::IntRect(0, Sprite_Left, Sprite_Width, Sprite_Height));
 
-		//Clearing screen
-		App->clear(sf::Color(0, 0, 0, 0));
+        }
+        if (rightFlag){
+            x+=SPRITE_SPEED;
+//            sprite.setTextureRect(sf::IntRect(0, Sprite_Right, Sprite_Width, Sprite_Height));
+        }
+        if (upFlag){
+            y-=SPRITE_SPEED;
+//            sprite.setTextureRect(sf::IntRect(0, Sprite_Back, Sprite_Width, Sprite_Height));
+
+
+        }
+        if (downFlag){
+            y+=SPRITE_SPEED;
+//            sprite.setTextureRect(sf::IntRect(0, Sprite_Front, Sprite_Width, Sprite_Height));
+        }
+
+        // Check screen boundaries
+        if (x<0) x=0;
+        if (x>(float)App->getSize().x) x=App->getSize().x;
+        if (y<0) y=0;
+        if (y>(float)App->getSize().y) y=App->getSize().y;
+
+        // Clear the window and apply grey background
+        App->clear( sf::Color(127,127,127));
+
+        // Rotate and draw the sprite
+        sprite.setPosition(x,y);
 		//Drawing
-		App->draw(Rectangle);
+		App->draw(sprite);
 		App->display();
 	}
 
