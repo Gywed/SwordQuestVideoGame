1721461595598352da2572c679b694a1be1c1ed9
adujacqu <alex.dujacquier@gmail.com>
2022-11-11 16:50:48 +0100

    create GameManagerView

---
+++ include/GameManagerView.h
@@ -0,0 +1,32 @@
+#ifndef GAMEMANAGERVIEW_H
+#define GAMEMANAGERVIEW_H
+
+
+class GameManagerView
+{
+    private:
+        // Window game
+        sf::RenderWindow* window;
+        // State(GameState,MenuState,etc..)
+        State* state;
+
+    public:
+        GameManagerView();
+        virtual ~GameManagerView();
+        GameManagerView(const GameManagerView& other);
+        GameManagerView& operator=(const GameManagerView& other);
+        /* Getters and setters */
+        sf::RenderWindow* getWindow() const;
+        State* getState() const;
+        void setWindow(sf::RenderWindow* window);
+        void setState(const EnumState st);
+        /*  Method  */
+        void update();
+        void render();
+
+    protected:
+
+
+};
+
+#endif // GAMEMANAGERVIEW_H
---
+++ src/GameManagerView.cpp
@@ -0,0 +1,23 @@
+#include "GameManagerView.h"
+
+GameManagerView::GameManagerView()
+{
+    //ctor
+}
+
+GameManagerView::~GameManagerView()
+{
+    //dtor
+}
+
+GameManagerView::GameManagerView(const GameManagerView& other)
+{
+    //copy ctor
+}
+
+GameManagerView& GameManagerView::operator=(const GameManagerView& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
